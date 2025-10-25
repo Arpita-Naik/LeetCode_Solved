80392390c21f6609ac3e5533bbae4741054741f4
Arpita Naik <arpitan462@gmail.com>
2025-10-26 05:24:21 +0530

    Added solution for product_of_array_except_itself.c

---
+++ Medium/product_of_array_except_itself.c
@@ -0,0 +1,41 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+/*int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    int *res=(int*)malloc(numsSize*sizeof(int));
+    *returnSize=numsSize;
+
+
+    for(int i=0;i<numsSize;i++)
+    {
+        res[i]=1;
+        for(int j=0;j<numsSize;j++)
+        {
+            if(i!=j)
+            {
+                res[i]*=nums[j];
+            }
+        }
+    }
+    return res;
+}*/ 
+
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    int *res=(int*)malloc(numsSize*sizeof(int));
+    *returnSize=numsSize;
+
+   //prefix products(left to right)
+    res[0]=1;
+    for(int i=1;i<numsSize;i++)
+    {
+        res[i]=res[i-1]*nums[i-1];
+    }
+    
+    int right=1;
+    for(int i=numsSize-1;i>=0;i--)
+    {
+        res[i]*=right;
+        right*=nums[i];
+    }
+    return res;
+}
