9798f0b53935ab90fd1a6360ea3999bcbe7c1ed9
Arpita Naik <arpitan462@gmail.com>
2025-10-30 05:16:17 +0530

    Added solution for increment_subarray.c

---
+++ Hard/increment_subarray.c
@@ -0,0 +1,12 @@
+int minNumberOperations(int* target, int targetSize) {
+    int ans=target[0];
+    for(int i=1;i<targetSize;i++)
+    {
+        //ans+=fmax(target[i]-target[i-1],0);
+        if(target[i]>target[i-1])
+        {
+            ans+=target[i]-target[i-1];
+        }
+    }
+    return ans;
+}
